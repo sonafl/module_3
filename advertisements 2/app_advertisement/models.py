5bbc56cc012c6bc11984934a7c21f4992d893915
mcndrw <narek.mcandrew@gmail.com>
2023-10-08 13:42:59 +0300

    project

---
+++ advertisements 2/app_advertisement/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+# Заголовок, цена, описание, дата создания, дата обновления, уместен ли торг 
+
+class Advertisement(models.Model):
+    # СharField - строковое поле для небольших строк 
+    title = models.CharField('Заголовок', max_length=128)
+    # TextField - строковое поле для больших строк 
+    description = models.TextField('Описание')
+    # 22300,55
+    # DecimalField - похоже на float в Питоне - числовое поле 
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    # BooleanField - логическое поле, принимающее 2 значения: True/False
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг будет уместен')
+    # DateTimeField - cпециальное поле для работы с датами 
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
