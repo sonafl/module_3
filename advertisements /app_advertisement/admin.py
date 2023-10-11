76e0c9f807439c2440eea4375a541a3b58e56133
mcndrw <narek.mcandrew@gmail.com>
2023-10-11 19:39:31 +0300

    admin

---
+++ advertisements /app_advertisement/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Advertisement
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']
+    list_filter = ['created_at', 'price']
+    actions = ['make_auction_false', 'make_auction_true']
+    fieldsets = (
+        ('Общее', {
+            'fields' : ('title', 'description'), 
+            'classes' : ['collapse']
+        }), 
+
+        ('Финансы', {
+            'fields' : ('price', 'auction'), 
+            'classes' : ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать все торги')
+    def make_auction_false(self, request, queryset):
+        queryset.update(auction = False)
+
+    @admin.action(description='Проставить возможность торгов')
+    def make_auction_true(self, request, queryset):
+        queryset.update(auction = True)
+
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+# Register your models here.
--- advertisements /app_advertisement/models.py
+++ advertisements /app_advertisement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 # Create your models here.
 # Заголовок, цена, описание, дата создания, дата обновления, уместен ли торг 
 
@@ -17,6 +18,17 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата обновления', auto_now=True)
   
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+    
     def __str__(self):
         return f'Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})'
     
