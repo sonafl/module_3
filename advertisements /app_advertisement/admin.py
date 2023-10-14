d827c465f21ff2af7e6891a4503c63636157ac60
mcndrw <narek.mcandrew@gmail.com>
2023-10-14 12:46:23 +0300

    project

--- advertisements /app_advertisement/admin.py
+++ advertisements /app_advertisement/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction', 'updated_date']
     list_filter = ['created_at', 'price']
     actions = ['make_auction_false', 'make_auction_true']
     fieldsets = (
