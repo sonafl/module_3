bdd424bf9889c0dc8f39a52f8c3a673a72bae06e
mcndrw <narek.mcandrew@gmail.com>
2023-10-18 12:02:21 +0300

    templates

---
+++ advertisements — /app_advertisement/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Advertisement
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction', 'get_html_image', 'updated_date']
+    list_filter = ['created_at', 'price']
+    actions = ['make_auction_false', 'make_auction_true']
+    fieldsets = (
+        ('Общее', {
+            'fields' : ('title', 'description', 'image'), 
+            'classes' : ['collapse']
+        }), 
+
+        ('Финансы', {
+            'fields' : ('price', 'auction'), 
+            'classes' : ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать все торги')
+    def make_auction_false(self, request, queryset):
+        queryset.update(auction = False)
+
+    @admin.action(description='Проставить возможность торгов')
+    def make_auction_true(self, request, queryset):
+        queryset.update(auction = True)
+
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+# Register your models here.
